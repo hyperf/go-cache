ac5220b8cdcca9d0b434deb143ca8f0a530c8929
李铭昕 <715557344@qq.com>
2024-12-16 11:00:22 +0800

    完善单测

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -2,13 +2,17 @@ package cache
 
 import (
 	"errors"
+	"github.com/hyperf/go-cache/driver/go_zero_driver"
 	"github.com/hyperf/go-cache/driver/null_driver"
 	"github.com/hyperf/go-cache/error_code"
 	"github.com/stretchr/testify/assert"
+	"github.com/zeromicro/go-zero/core/stores/redis"
+	"strconv"
 	"testing"
+	"time"
 )
 
-func TestCache_Get(t *testing.T) {
+func TestCacheNullDriver(t *testing.T) {
 	res := &Cache[*Foo]{Driver: null_driver.NullDriver{}, Packer: &JsonPacker{}}
 
 	var result Foo
@@ -19,4 +23,38 @@ func TestCache_Get(t *testing.T) {
 	result2 := &Foo{Id: 10}
 	_ = res.Get("xx", result2)
 	assert.Equal(t, 10, result2.Id)
+}
+
+func TestCacheGoZeroDriver(t *testing.T) {
+	driver := go_zero_driver.NewGoZeroDriver(redis.MustNewRedis(redis.RedisConf{
+		Host: "127.0.0.1:6379",
+		Type: "node",
+	}))
+
+	fooCache := &Cache[*Foo]{Driver: driver, Packer: &JsonPacker{}}
+	key := "c:" + strconv.FormatInt(time.Now().Unix(), 10)
+
+	var result Foo
+	err := fooCache.Get(key, &result)
+	assert.Equal(t, 0, result.Id)
+	assert.True(t, errors.Is(err, error_code.NotFound))
+
+	res := &Foo{Id: 10}
+	_ = fooCache.Get(key, res)
+	assert.Equal(t, 10, res.Id)
+
+	res.Id = 100
+	exists, _ := fooCache.Has(key)
+	assert.False(t, exists)
+
+	err = fooCache.Set(key, res, 60)
+	assert.Nil(t, err)
+
+	exists, _ = fooCache.Has(key)
+	assert.True(t, exists)
+
+	err = fooCache.Get(key, &result)
+	assert.Nil(t, err)
+
+	assert.Equal(t, 100, result.Id)
 }
